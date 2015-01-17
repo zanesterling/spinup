712cf70442c27571268b31276ef30550e5e9b95a
mgouzenko <mgouzenko@gmail.com>
2015-01-17 02:26:12 -0500

    formatted auth url

--- server/server.py
+++ server/server.py
@@ -1,8 +1,12 @@
 from flask import Flask, session, request, render_template
 from pymongo import MongoClient
+import request
 
 app = Flask(__name__)
 db = MongoClient().spinup
+CLIENT_ID = 'bb93565e1f2db84beeb740a0d704d820fb93f1a5db4984050b23121dfe583a7b'
+CLIENT_SECRET = 'e0fba3d9cf155f3cef4ae275e7abb3c05fb5c157260beec55a15ef2acc88bec9'
+CALLBACK = 'http://104.131.75.88:9001/callback'
 
 # webpage and ui
 @app.route('/')
@@ -12,8 +16,16 @@ def home():
 
 @app.route('/callback', methods=['GET', 'POST'])
 def authenticate():
-    print request.data
-    return 'yo'
+    code = request.args['code']
+    url =   '''https://cloud.digitalocean.com/v1/oauth/token'''
+    '''?client_id=%(client_id)s'''
+    '''&client_secret=%(client_secret)s''' 
+    '''&code=%(code)s&'''
+    '''grant_type=authorization_code&'''
+    '''redirect_uri=%(callback_URL)s''' %{'client_id': CLIENT_ID, "client_secret": CLIENT_SECRET,
+                                              "code":code,
+                                              "callback_URL": CALLBACK}
+    return url
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
