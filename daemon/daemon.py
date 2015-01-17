de00c7d4363d29d30ac3e105270d726d5dc8f146
Zane Sterling <sterling.zane@gmail.com>
2015-01-17 02:57:03 -0500

    Make daemon, test_client a bit more readable

---
+++ daemon/daemon.py
@@ -0,0 +1,74 @@
+import socket
+import threading
+import json
+import time
+import datetime
+
+DATE_FORMAT = "%m:%d:%y:%H:%M:%S"
+PORT = 1234
+
+class Daemon(object):
+    def __init__(self):
+        self.sock = socket.socket()
+        self.sock.bind(('localhost', PORT))
+        self.sock.listen(5)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.cache = []
+    
+    def listen(self):
+        def listen_thread():
+            while True:
+				# accept a connection
+                conn, addr = self.sock.accept()
+
+                # load data from socket
+                data = conn.recv(4096)
+                payload = {
+                    "timestamp": datetime.datetime.now().strftime(DATE_FORMAT),
+                    "data": json.loads(data)
+                }
+
+				# if the payload can be merged into the cache
+                if shouldMergePayloads(payload):
+                    # merge it
+                    print "merged"
+                    for k in payload["data"].keys():
+                        self.cache[-1]["data"][k] = payload["data"][k]
+                else:
+                    # append it to the cache
+                    self.cache.append(payload)
+                    print "appended"
+                if 'cmd' in payload['data']:
+                    if payload['data']['cmd'] == 'die':
+                        break
+                conn.close()
+                print self.cache
+                time.sleep(0.001)
+
+        def shouldMergePayloads(payload):
+            # if there's nothing to merge with, return
+            if len(self.cache) == 0:
+                print "cache empty"
+                return False
+            
+            # if the payload and the cached payload have different timestamps, return
+            lastInCache = self.cache[-1]
+            if lastInCache["timestamp"] != payload["timestamp"]:
+                print "timestamps differ"
+                return False
+            
+            for k in payload["data"].keys():
+                if k in lastInCache["data"].keys():
+                    print "keys overlap"
+                    return False
+
+            print "all good"
+            return True
+
+        threading.Thread(group=None, target=listen_thread).start()
+
+
+
+if __name__ == '__main__':
+    daemon = Daemon()
+    daemon.listen()
