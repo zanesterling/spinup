a09256545a59682b1eec495ce3712d1fb8a56ed1
Josh Hofing <joshhofing@gmail.com>
2015-01-17 04:15:21 -0500

    Add cpu polling and dynamic dormancy

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -4,8 +4,11 @@ import json
 import time
 import datetime
 
+import performance
+
 DATE_FORMAT = "%m:%d:%y:%H:%M:%S"
 PORT = 1234
+BASE_SLEEP_TIME = 5
 
 class Daemon(object):
     def __init__(self):
@@ -17,38 +20,40 @@ class Daemon(object):
     
     def listen(self):
         def listen_thread():
-            while True:
+            while self.running:
 				# accept a connection
                 conn, addr = self.sock.accept()
 
                 # load data from socket
-                data = conn.recv(4096)
-                payload = {
-                    "timestamp": datetime.datetime.now().strftime(DATE_FORMAT),
-                    "data": json.loads(data)
-                }
-
-				# if the payload can be merged into the cache
-                if shouldMergePayloads(payload):
-                    # merge it
-                    print "merged" #debug
-                    for k in payload["data"].keys():
-                        self.cache[-1]["data"][k] = payload["data"][k]
-                else:
-                    # append it to the cache
-                    self.cache.append(payload)
-                    print "appended" #debug
-
+                data = json.loads(conn.recv(4096))
+                self.receive_msg(data)
 				# accept commands from sender
-                if 'cmd' in payload['data']:
-                    if payload['data']['cmd'] == 'die':
-                        break
+                if 'cmd' in data:
+                    if data['cmd'] == 'die':
+                        self.running = False
 
 				# wrap up and prepare to sleep until the next cycle
                 conn.close()
                 print self.cache #debug
                 time.sleep(0.001)
 
+        def cpu_polling_thread():
+            while self.running:
+                cpu = performance.get_CPU_usage()
+                ram = performance.get_RAM_usage()
+                self.receive_msg({
+                    "cpu": cpu,
+                    "ram": ram
+                    })
+                percent_to_sleep = 100 - max(cpu, ram)
+                time.sleep((percent_to_sleep / 100.0) * BASE_SLEEP_TIME)
+
+
+        self.running = True
+        threading.Thread(group=None, target=listen_thread).start()
+        threading.Thread(group=None, target=cpu_polling_thread).start()
+
+    def receive_msg(self, data):
         def shouldMergePayloads(payload):
             # if there's nothing to merge with, return
             if len(self.cache) == 0:
@@ -70,7 +75,21 @@ class Daemon(object):
             #print "all good"
             return True
 
-        threading.Thread(group=None, target=listen_thread).start()
+        payload = {
+                "timestamp": datetime.datetime.now().strftime(DATE_FORMAT),
+                "data": data
+                }
+
+        # if the payload can be merged into the cache
+        if shouldMergePayloads(payload):
+            # merge it
+            print "merged" #debug
+            for k in payload["data"].keys():
+                self.cache[-1]["data"][k] = payload["data"][k]
+        else:
+            # append it to the cache
+            self.cache.append(payload)
+            print "appended" #debug
 
 if __name__ == '__main__':
     daemon = Daemon()
