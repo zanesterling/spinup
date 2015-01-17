7aa6afba7543f5456fa20f25d20627723455fb4b
Zane Sterling <sterling.zane@gmail.com>
2015-01-16 22:49:37 -0500

    Fix syntax issues

--- load-balancer/load-balancer.py
+++ load-balancer/load-balancer.py
@@ -8,7 +8,7 @@ db = MongoClient().loadBalancer
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-	childToCall = getNextServer() + '/' + path
+	childToCall = str(getNextServer()) + '/' + path
 	return childToCall
 	# forward request to child server
 	# return result of forward
@@ -21,10 +21,10 @@ def getNextServer():
 	serverIndex = db.config.find_one()['serverIndex']
 	nextServer = None
 	if len(servers) > 0:
-		nextServer = servers[serverIndex]['url']
+		nextServer = servers[serverIndex % len(servers)]['url']
 
-	# increment serverIndex in the db
-	db.config.update({}, {'serverIndex': (serverIndex + 1) % len(servers)}) 
+		# increment serverIndex in the db
+		db.config.update({}, {'serverIndex': (serverIndex + 1) % len(servers)}) 
 
 	# send back the server
 	return nextServer
