f403bc43a3c43e6269112f701cf5c599bf37fd0c
Josh Hofing <joshhofing@gmail.com>
2015-01-16 21:24:48 -0500

    Add performance checking

---
+++ performance.py
@@ -0,0 +1,14 @@
+import psutil
+
+# CAUTION: A
+def get_CPU_usage():
+    return psutil.cpu_percent(interval=1)
+
+def get_RAM_usage():
+    mem = psutil.virtual_memory()
+    return 100 * float(mem.used) / float(mem.total)
+
+
+if __name__ == '__main__':
+    print get_CPU_usage()
+    print get_RAM_usage()
