41d9f6f15a813842ae15e0960eb1c613ec8324ea
Zane Sterling <sterling.zane@gmail.com>
2015-01-16 21:29:11 -0500

    Switch to render_template, add service route

--- server/server.py
+++ server/server.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# webpage and ui
 @app.route('/')
 def home():
+	return render_template('home.html')
 	return 'You\'ve found the webpage for Spinup!'
 
+# daemon interaction
+@app.route('/service', methods=['POST'])
+def service():
+	return 'accepted'
+
 if __name__ == '__main__':
-	app.run('0.0.0.0', 9001)
+	app.run('0.0.0.0', 9001, debug=True)
