b9669f9be38f965e48fe83214b8ba355fea61d59
Zane Sterling <sterling.zane@gmail.com>
2015-01-17 13:32:48 -0500

    Add lines between adjacent nodes

--- server/static/stats.js
+++ server/static/stats.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var width = svg.width.baseVal.value;
     var height = svg.height.baseVal.value;
 
+    // add nodes for data
     d3.select('svg')
         .selectAll('circle')
         .data(datasets[0])
@@ -19,4 +20,21 @@ $(document).ready(function() {
             .attr('fill', '#6f96a2')
             .attr('stroke', 'white')
             .attr('stroke-width', 2);
+
+    // add lines between adjacent nodes
+    d3.select('svg')
+        .selectAll('circle')[0]
+        .forEach(function(d, i, a) {
+            if (i == 0) return; // can't draw backwards from origin
+
+            // add a line from this node to the previous
+            d3.select('svg')
+                .append('line')
+                    .attr('x1', d.cx.baseVal.value)
+                    .attr('y1', d.cy.baseVal.value)
+                    .attr('x2', a[i-1].cx.baseVal.value)
+                    .attr('y2', a[i-1].cy.baseVal.value)
+                    .attr('stroke', 'white')
+                    .attr('stroke-width', 2);
+        });
 });
